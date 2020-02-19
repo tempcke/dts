3499891906945a2cd5afe99128c9c5c54eed9cbd
tempcke <t.github@logikostech.net>
2020-02-19 10:02:01 -0600

    abstract persistence to allow for key maping and converting to/from Datetime etc

---
+++ src/DTS/Persistence/AbstractPersistence.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace HomeCEU\DTS\Persistence;
+
+
+use HomeCEU\DTS\Persistence;
+
+abstract class AbstractPersistence implements Persistence {
+  /** @var array */
+  private $hydratedToDbMap;
+  /** @var array */
+  private $dbToHydratedMap;
+
+  /**
+   * @param array $map map of hydratedKey => db_key
+   */
+  public function useKeyMap(array $map) {
+    $this->hydratedToDbMap = $map;
+    $this->dbToHydratedMap = array_flip($map);
+  }
+
+  protected function hydratedKey($db_key) {
+    if (array_key_exists($db_key, $this->dbToHydratedMap)) {
+      return $this->dbToHydratedMap[$db_key];
+    }
+    return $db_key;
+  }
+  protected function dbKey($hydratedKey) {
+    if (array_key_exists($hydratedKey, $this->hydratedToDbMap)) {
+      return $this->hydratedToDbMap[$hydratedKey];
+    }
+    return $hydratedKey;
+  }
+
+  public function flatten(array $entity) {
+    $result = array();
+
+    foreach ($entity as $k => $v) {
+      if ($v instanceof \DateTime) {
+        $value = $v->format('Y-m-d H:i:s');
+      } elseif (is_array($v)) {
+        $value = json_encode($v);
+      } else {
+        $value = $v;
+      }
+      $key = $this->dbKey($k);
+      $result[$key] = $value;
+    }
+
+    return $result;
+  }
+
+  public function hydrate(array $entity) {
+    $result = array();
+
+    foreach ($entity as $k => $v) {
+      $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $v);
+      if ($dateTime !== FALSE) {
+        $value = $dateTime;
+      } elseif ($this->isJson($v)) {
+        $value = $this->jsonDecodeAsArray($v);
+      } else {
+        $value = $v;
+      }
+      $key = $this->hydratedKey($k);
+      $result[$key] = $value;
+    }
+
+    return $result;
+  }
+
+  private function isJson($v) {
+    return is_array(json_decode($v, true));
+  }
+
+  private function jsonDecodeAsArray($json): array {
+    return json_decode($json, true);
+  }
+
+  // CREDIT: https://stackoverflow.com/a/19533226/2683059
+  protected function toSnakeCase($input) {
+    return ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $input)), '_');
+  }
+}
---
+++ tests/DTS/Persistence/AbstractPersistenceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace HomeCEU\Tests\DTS\Persistence;
+
+
+use HomeCEU\DTS\Persistence\AbstractPersistence;
+use HomeCEU\Tests\DTS\TestCase;
+use PHPUnit\Framework\Assert;
+
+class AbstractPersistenceTest extends TestCase {
+  public function hydrated() {
+    return [
+        "firstName" => "Fred",
+        "birthDay" => new \DateTime("2000-01-01"),
+        "location" => ["state" => "TX", "city" => "Dallas"]
+    ];
+  }
+
+  public function mySqlForm() {
+    return [
+        "first_name" => "Fred",
+        "birth_day" => "2000-01-01 00:00:00",
+        "location" => '{"state":"TX","city":"Dallas"}'
+    ];
+  }
+
+  public function keymap() {
+    return [
+        'firstName' => 'first_name',
+        'lastName' => 'last_name',
+        'birthDay'  => 'birth_day',
+    ];
+  }
+
+  public function testFlatten() {
+    $p = $this->persistence();
+    $p->useKeyMap($this->keymap());
+    $flattened = $p->flatten($this->hydrated());
+    Assert::assertEquals($this->mySqlForm(), $flattened);
+  }
+
+  public function testHydrateFromMySQL() {
+    $p = $this->persistence();
+    $p->useKeyMap($this->keymap());
+    $hydrated = $p->hydrate($this->mySqlForm());
+    Assert::assertEquals($this->hydrated(), $hydrated);
+  }
+
+  protected function persistence() {
+    return new class extends AbstractPersistence {
+      public function generateId() {}
+      public function persist($data) {}
+      public function retrieve($id) {}
+      public function delete($id) {}
+      public function find(array $filter) {}
+    };
+  }
+}
