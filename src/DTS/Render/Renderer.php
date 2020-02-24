545ec40ab8546d97c7c18919d5d73ffa189fe0f7
Dan McAdams <danmcadams@gmail.com>
2020-02-24 17:25:35 -0600

    format fix

--- src/DTS/Render/Renderer.php
+++ src/DTS/Render/Renderer.php
@@ -6,16 +6,17 @@ namespace HomeCEU\DTS\Render;
 use LightnCandy\Flags;
 use LightnCandy\LightnCandy;
 
-class Renderer {
+class Renderer
+{
 
-  /** @var string */
-  private $template;
-  /** @var Partial[] */
-  private $partials = [];
-  /** @var Helper[] */
-  private $helpers = [];
-  /** @var int */
-  private $flags = Flags::FLAG_HANDLEBARS;
+    /** @var string */
+    private $template;
+    /** @var Partial[] */
+    private $partials = [];
+    /** @var Helper[] */
+    private $helpers = [];
+    /** @var int */
+    private $flags = Flags::FLAG_HANDLEBARS;
 
     protected function __construct()
     {
@@ -28,96 +29,96 @@ class Renderer {
         return new self();
     }
 
-  public function setFlags(int $flags): void
-  {
-    $this->flags = $flags;
-  }
-
-  public function addPartial(Partial $partial): void
-  {
-    $this->partials[$partial->name] = $partial->template;
-  }
-
-  public function setPartials(array $partials): void
-  {
-    $this->resetPartials();
-    foreach ($partials as $partial) {
-      $this->addPartial($partial);
+    public function setFlags(int $flags): void
+    {
+        $this->flags = $flags;
     }
-  }
 
-  public function addHelper(Helper $helper): void
-  {
-    $this->helpers[$helper->name] = $helper->func;
-  }
-
-  public function setHelpers(array $helpers): void
-  {
-    $this->resetHelpers();
-    foreach ($helpers as $helper) {
-      $this->addHelper($helper);
+    public function addPartial(Partial $partial): void
+    {
+        $this->partials[$partial->name] = $partial->template;
     }
-  }
 
-  public function setTemplate(string $template): void
-  {
-    $this->template = $template;
-  }
-
-  public function render(array $data): ?string
-  {
-    if (empty($this->template)) {
-      throw new NoTemplateProvidedException('Cannot render template, no template was provided.');
+    public function setPartials(array $partials): void
+    {
+        $this->resetPartials();
+        foreach ($partials as $partial) {
+            $this->addPartial($partial);
+        }
     }
-    return $this->renderCompiledTemplate($this->compileTemplate(), $data);
-  }
 
-  private function compileTemplate()
-  {
-    return LightnCandy::compile($this->template, $this->buildOptions());
-  }
+    public function addHelper(Helper $helper): void
+    {
+        $this->helpers[$helper->name] = $helper->func;
+    }
 
-  public function renderCompiledTemplate(string $compiledTemplate, array $data): ?string
-  {
-    $file = $this->saveCompiledTemplateToTempFile($compiledTemplate);
+    public function setHelpers(array $helpers): void
+    {
+        $this->resetHelpers();
+        foreach ($helpers as $helper) {
+            $this->addHelper($helper);
+        }
+    }
 
-    $fn = include($file);
-    unlink($file);
+    public function setTemplate(string $template): void
+    {
+        $this->template = $template;
+    }
 
-    return trim($fn($data));
-  }
+    public function render(array $data): ?string
+    {
+        if (empty($this->template)) {
+            throw new NoTemplateProvidedException('Cannot render template, no template was provided.');
+        }
+        return $this->renderCompiledTemplate($this->compileTemplate(), $data);
+    }
 
-  private function saveCompiledTemplateToTempFile(string $compiledTemplate): string
-  {
-    $fileName = tempnam(sys_get_temp_dir(), 'cert_');
-    file_put_contents($fileName, "<?php {$compiledTemplate}");
+    private function compileTemplate()
+    {
+        return LightnCandy::compile($this->template, $this->buildOptions());
+    }
 
-    return $fileName;
-  }
+    public function renderCompiledTemplate(string $compiledTemplate, array $data): ?string
+    {
+        $file = $this->saveCompiledTemplateToTempFile($compiledTemplate);
 
-  protected function buildOptions(): array
-  {
-    return [
-        'flags' => $this->flags | Flags::FLAG_ERROR_EXCEPTION,
-        'helpers' => $this->helpers,
-        'partials' => $this->partials
-    ];
-  }
+        $fn = include($file);
+        unlink($file);
 
-  private function resetPartials(): void
-  {
-    $this->partials = [];
-  }
+        return trim($fn($data));
+    }
 
-  private function resetHelpers(): void
-  {
-    $this->helpers = [];
-    $this->setDefaultHelpers();
-  }
+    private function saveCompiledTemplateToTempFile(string $compiledTemplate): string
+    {
+        $fileName = tempnam(sys_get_temp_dir(), 'cert_');
+        file_put_contents($fileName, "<?php {$compiledTemplate}");
 
-  private function setDefaultHelpers(): void
-  {
-    $ifComparisonHelper = Helpers::ifComparisonHelper();
-    $this->helpers[$ifComparisonHelper->name] = $ifComparisonHelper->func;
-  }
+        return $fileName;
+    }
+
+    protected function buildOptions(): array
+    {
+        return [
+            'flags'    => $this->flags | Flags::FLAG_ERROR_EXCEPTION,
+            'helpers'  => $this->helpers,
+            'partials' => $this->partials
+        ];
+    }
+
+    private function resetPartials(): void
+    {
+        $this->partials = [];
+    }
+
+    private function resetHelpers(): void
+    {
+        $this->helpers = [];
+        $this->setDefaultHelpers();
+    }
+
+    private function setDefaultHelpers(): void
+    {
+        $ifComparisonHelper = Helpers::ifComparisonHelper();
+        $this->helpers[$ifComparisonHelper->name] = $ifComparisonHelper->func;
+    }
 }
