77dc9b40baf5dc72889da9237cede9d488c049f0
dan <danmcadams@gmail.com>
2020-02-25 16:05:24 -0600

    add migration for templates

---
+++ db/migrations/20200225185308_template_migration.php
@@ -0,0 +1,65 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class TemplateMigration extends AbstractMigration {
+  protected $templateDir;
+  protected $partialDir;
+
+  protected function init() {
+    parent::init();
+
+    if (!defined('APP_ROOT')) {
+      define('APP_ROOT', realpath(__DIR__ . '/../../'));
+    }
+    $this->templateDir = APP_ROOT . '/temp_templates';
+    $this->partialDir = $this->templateDir . '/accreditation_partials';
+  }
+
+  public function up() {
+    $templates = array_merge(
+        $this->extractTemplates($this->templateDir, 'certificate'),
+        $this->extractTemplates($this->partialDir, 'certificate/partial')
+    );
+    if (!empty($templates)) {
+      $this->table('template')->insert($templates)->save();
+    }
+  }
+
+  public function down() {
+    $this->execute('DELETE FROM template;');
+  }
+
+  private function extractTemplates($location, $docType) {
+    $templates = [];
+    foreach (scandir($location) as $file) {
+      if ($this->fileIsHidden($file)) {
+        continue;
+      }
+      $path = "$location/{$file}";
+      $pathInfo = pathinfo($path);
+
+      if ($pathInfo['extension'] == 'template') {
+        $body = file_get_contents($path);
+        $templates[] = $this->getTemplateArray($pathInfo, $body, $docType);
+      }
+    }
+    return $templates;
+  }
+
+  private function fileIsHidden($file): bool {
+    return $file[0] == '.';
+  }
+
+  private function getTemplateArray($pathInfo, string $body, $docType): array {
+    return [
+        'body' => $body,
+        'template_key' => str_replace('.', '-', $pathInfo['filename']),
+        'template_id' => \Ramsey\Uuid\Uuid::uuid1(),
+        'name' => ucwords(str_replace(['.', '_'], ' ', $pathInfo['filename'])),
+        'author' => 'System',
+        'doc_type' => $docType,
+        'created_at' => (new \DateTime())->format('Y-m-d H:i:s'),
+    ];
+  }
+}
