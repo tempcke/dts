c1426f9cc58438007f652aeda9e15e47246f96a2
Dan McAdams <danmcadams@gmail.com>
2020-03-01 23:53:42 -0600

    change if helper to comp-if

--- src/DTS/Render/TemplateHelpers.php
+++ src/DTS/Render/TemplateHelpers.php
@@ -6,7 +6,7 @@ namespace HomeCEU\DTS\Render;
 
 class TemplateHelpers {
   public static function ifComparisonHelper(): Helper {
-    return new Helper('if', function ($arg1, $arg2, $return) {
+    return new Helper('comp-if', function ($arg1, $arg2, $return) {
       return $arg1 == $arg2 ? $return : '';
     });
   }
