8274b0ba4a59008920f5bd9702aa3f01d1d83aef
Dan McAdams <danmcadams@gmail.com>
2020-03-11 10:39:15 -0500

    create status endpoint, returns a 200 if service is up and accessible

---
+++ api/Status.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\DTS\Api;
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Status {
+  public function __invoke(Request $request, Response $response, $args) {
+    return $response->withStatus(200);
+  }
+}
