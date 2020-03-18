ed858de71a9c416e55662cdce33387cd944a70bf
dan <danmcadams@gmail.com>
2020-03-18 16:04:02 -0500

    CEMS-1742 return 409 when missing keys or data for docType and dataKey when attemping to add a docdata record

--- api/DocData/DocDataAdd.php
+++ api/DocData/DocDataAdd.php
@@ -5,10 +5,12 @@ namespace HomeCEU\DTS\Api\DocData;
 
 
 use HomeCEU\DTS\Api\DiContainer;
+use HomeCEU\DTS\Entity\DocData;
 use HomeCEU\DTS\Persistence;
 use HomeCEU\DTS\Persistence\DocDataPersistence;
 use HomeCEU\DTS\Repository\DocDataRepository;
 use HomeCEU\DTS\UseCase\AddDocData;
+use HomeCEU\DTS\UseCase\InvalidDocDataAddRequestException;
 use Psr\Container\ContainerInterface;
 use Slim\Http\Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -35,21 +37,25 @@ class DocDataAdd {
   }
 
   public function __invoke(Request $request, Response $response, $args) {
-    $reqData = $request->getParsedBody();
-    $docData = $this->useCase->add(
-        $reqData['docType'],
-        $reqData['dataKey'],
-        $reqData['data']
-    );
-    $savedDocData = $this->persistence->retrieve(
-        $docData['dataId'],
-        [
-            'dataId',
-            'docType',
-            'dataKey',
-            'createdAt'
-        ]
-    );
-    return $response->withStatus(201)->withJson($savedDocData);
+    try {
+      $reqData = $request->getParsedBody();
+      $docData = $this->useCase->add(
+          $reqData['docType'],
+          $reqData['dataKey'],
+          $reqData['data']
+      );
+      $savedDocData = $this->persistence->retrieve(
+          $docData['dataId'],
+          [
+              'dataId',
+              'docType',
+              'dataKey',
+              'createdAt'
+          ]
+      );
+      return $response->withStatus(201)->withJson($savedDocData);
+    } catch (InvalidDocDataAddRequestException $e) {
+      return $response->withStatus(409)->withJson(['errors' => 'one or more required keys are missing. "docType", "dataKey", "data"']);
+    }
   }
 }
