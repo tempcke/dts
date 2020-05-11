804bccb1abcdebe696c0d5e80f5837140ddef8ca
dan <danmcadams@gmail.com>
2020-05-11 16:44:38 -0500

    CEMS-1814 add update admin-approved template to check for a company logo url, if one is provided, display that instead of the default logo

--- src/DTS/Render/RenderHelper.php
+++ src/DTS/Render/RenderHelper.php
@@ -9,4 +9,8 @@ class RenderHelper {
     preg_match_all('/{{>([^\}}]+)}}/', $template, $matches);
     return !empty($matches[1]) ? array_map('trim', $matches[1]) : [];
   }
+  public static function base_64_image()
+  {
+
+  }
 }
