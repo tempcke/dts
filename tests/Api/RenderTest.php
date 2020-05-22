136384cf8a837bdcc7e16cb3eb8a623f70197572
dan <danmcadams@gmail.com>
2020-05-22 17:11:14 -0500

    CEMS-1861 render html as default

--- tests/Api/RenderTest.php
+++ tests/Api/RenderTest.php
@@ -25,8 +25,8 @@ class RenderTest extends TestCase {
     $this->addTemplateFixture($templateKey);
     $response = $this->get("/render/{$this->docType}/{$templateKey}/{$dataKey}");
 
-    $this->assertContentType('application/pdf', $response);
     $this->assertStatus(200, $response);
-    Assert::assertInstanceOf(Stream::class, $response->getBody());
+    $this->assertContentType('text/html', $response);
+    $this->assertEquals("Hi Fred", (string) $response->getBody());
   }
 }
