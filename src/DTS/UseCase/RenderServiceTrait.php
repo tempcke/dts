bc182b03260e4aeeaa6d0e553e9fce64a6172ab7
Dan McAdams <danmcadams@gmail.com>
2020-06-02 00:32:12 -0500

    set initial format to null, in case a request has a null format, we still want to get an HTML renderer

--- src/DTS/UseCase/RenderServiceTrait.php
+++ src/DTS/UseCase/RenderServiceTrait.php
@@ -8,7 +8,7 @@ use HomeCEU\DTS\Render\RenderFactory;
 use HomeCEU\DTS\Render\RenderInterface;
 
 trait RenderServiceTrait {
-  protected function getRenderService(string $format): RenderInterface {
+  protected function getRenderService(string $format = null): RenderInterface {
     return ($format === RenderFormat::FORMAT_PDF)
         ? RenderFactory::createPDF()
         : RenderFactory::createHTML();
